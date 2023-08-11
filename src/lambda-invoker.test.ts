8964c662bc33cc4004840e8b1265fa10bd694dba
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-08-11 18:34:38 +0200

    adapt tests

--- src/lambda-invoker.test.ts
+++ src/lambda-invoker.test.ts
@@ -5,36 +5,39 @@ import { mockClient } from 'aws-sdk-client-mock'
 const mockLambdaClient = mockClient(Lambda)
 
 describe('Lambda invoker', () => {
-
   it('process 200 response with http error returned', async () => {
     // given
+    const payload = {
+      statusCode: 404,
+      body: {
+        message: 'User not found',
+      },
+    }
+
     mockLambdaClient.on(InvokeCommand).resolves({
       StatusCode: 200,
-      Payload: JSON.stringify({
-        statusCode: 404,
-        body: JSON.stringify({
-          message: 'User not found',
-        }),
-      }) as any
+      Payload: Buffer.from(JSON.stringify(payload)) as any
     })
    
     // then
     const resp = await invokeLambda({ payload: '', functionName: 'test' })
     expect(resp.statusCode).toEqual(404)
-    expect(resp.body).toEqual(JSON.stringify({
+    expect(resp.body).toEqual({
       message: 'User not found',
-    }))
+    })
   })
 
   it('process 200 response with timeout error returned', async () => {
     // given
+    const payload = {
+      errorMessage: "2023-01-09T10:48:53.262Z 873b04e4-991b-4d5f-b7ca-b99df84bfd66 Task timed out after 1.00 seconds"
+    }
     mockLambdaClient.on(InvokeCommand).resolves({
       StatusCode: 200,
       FunctionError: 'Unhandled',
-      Payload: JSON.stringify({
-        errorMessage: "2023-01-09T10:48:53.262Z 873b04e4-991b-4d5f-b7ca-b99df84bfd66 Task timed out after 1.00 seconds"
-      }) as any
+      Payload: Buffer.from(JSON.stringify(payload)) as any
     })
+    
 
     // then
     await expect(invokeLambda({ payload: '', functionName: 'test' })).rejects.toThrow()
@@ -42,15 +45,15 @@ describe('Lambda invoker', () => {
 
   it('process 200 response with non APIGatewayProxyStructuredResultV2 payload', async () => {
     // given
+    const payload = {
+      username: 'this should fail'
+    }
     mockLambdaClient.on(InvokeCommand).resolves({
       StatusCode: 200,
-      Payload: JSON.stringify({
-        username: 'this should fail'
-      }) as any
+      Payload: Buffer.from(JSON.stringify(payload)) as any
     })
 
     // then
     await expect(invokeLambda({ payload: '', functionName: 'test' })).rejects.toThrow()
   })
-
 })
